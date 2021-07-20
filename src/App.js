dabc529da82a42e8020974eb3604640bbaf1f96e
elbigans <rhafaelbijaksana04@gmail.com>
2021-07-20 15:24:08 +0700

    mengecheck tema dari localstroage

--- src/App.js
+++ src/App.js
@@ -28,8 +28,18 @@ const theme = {
   }
 };
 
+
 function App() {
-  const [mode, setMode] = useState(false);
+  const [mode, setMode] = useState(() => {
+    const theme = localStorage.getItem('theme');
+    return theme === "true" ? true : false;
+  });
+
+  // Masukan kedalam local
+  useEffect(() => {
+    localStorage.setItem("theme", mode ? "true" : "false");
+  }, [mode]);
+
   return (
     <ThemeProvider theme={mode ? theme.light : theme.dark}>
       <GlobalStyle />
@@ -224,6 +234,72 @@ function Footer(props) {
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${({theme}) => theme.box.backgroundColor};
+  };
+
+  .loader {
+    font-size: 10px;
+    margin: 50px auto;
+    text-indent: -9999em;
+    width: 5em;
+    height: 5em;
+    border-radius: 50%;
+    background: #764abc;
+    background: -moz-linear-gradient(
+      left,
+      #764abc 10%,
+      rgba(128, 0, 255, 0) 42%
+    );
+    background: -webkit-linear-gradient(
+      left,
+      #764abc 10%,
+      rgba(128, 0, 255, 0) 42%
+    );
+    background: -o-linear-gradient(
+      left,
+      #764abc 10%,
+      rgba(128, 0, 255, 0) 42%
+    );
+    background: -ms-linear-gradient(
+      left,
+      #764abc 10%,
+      rgba(128, 0, 255, 0) 42%
+    );
+    background: linear-gradient(
+      to right,
+      #764abc 10%,
+      rgba(128, 0, 255, 0) 42%
+    );
+    position: relative;
+    -webkit-animation: load3 1.4s infinite linear;
+    animation: load3 1.4s infinite linear;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+  }
+  
+  .loader:before {
+    width: 50%;
+    height: 50%;
+    background: #764abc;
+    border-radius: 100% 0 0 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: '';
+  }
+  
+  .loader:after {
+    background: ${({theme}) => theme.box.backgroundColor};
+    width: 75%;
+    height: 75%;
+    border-radius: 50%;
+    content: '';
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
   }
 `;
 
